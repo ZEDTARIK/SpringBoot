b74da353e4c0cff94837ffeab43f6f080c9a7838
zouhair <ettarak.zouhair@gmail.com>
2021-12-27 22:07:36 +0100

    Spring - generate Entity Contact

---
+++ src/main/java/com/ettarak/entities/Contact.java
@@ -0,0 +1,97 @@
+package com.ettarak.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Contact implements Serializable {
+	
+	private static final long serialVersionUID = -8854690784296962566L;
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String nom;
+	private String prenom;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dateNaissance;
+	
+	private String email;
+	private long tele;
+	private String photo;
+	
+	public Contact() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public Contact(String nom, String prenom, Date dateNaissance, String email, long tele, String photo) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateNaissance = dateNaissance;
+		this.email = email;
+		this.tele = tele;
+		this.photo = photo;
+	}
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getTele() {
+		return tele;
+	}
+	public void setTele(long tele) {
+		this.tele = tele;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+	
+	
+	
+
+	
+}
